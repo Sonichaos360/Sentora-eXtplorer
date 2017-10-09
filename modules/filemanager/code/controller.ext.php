1337b4e76b31933b3c5fe54fe5aae0d44d5a9c94
Luciano Vergara <shinichi.e.p@live.com>
2017-10-09 18:17:19 -0300

    Some fixes

--- modules/filemanager/code/controller.ext.php
+++ modules/filemanager/code/controller.ext.php
@@ -34,7 +34,7 @@ class module_controller extends ctrl_module
         $random = self::getRandomString(20);
         $random_pass = md5($random);
         $username = $currentuser["username"];
-        $file = '/etc/apps/filemanager/config/.htusers.php';
+        $file = '/etc/sentora/panel/etc/apps/filemanager/config/.htusers.php';
 
         //If exits remove old extplorer user
         $lines = file($file);
@@ -61,7 +61,7 @@ class module_controller extends ctrl_module
         $content = stream_get_contents(fopen($file, "r"));
 
         //Add user
-        $content = str_replace("//The array", "//The array \n         array('".$username."','".$random_pass."','/var/sentora/hostdata/".$username."','','1','','1',1),", $content);
+        $content = str_replace("//The array", "//The array \n         array('".$username."','".$random_pass."','/var/sentora/hostdata/".$username."/*','','1','','1',1),", $content);
 
         fwrite(fopen($file, "w"),$content);
 
